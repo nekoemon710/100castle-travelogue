3dc9362d0ec31f9ffb462d03ecd86e09cab3a392
nekoemon710 <neko.to.shisyamo@gmail.com>
2025-08-06 23:26:19 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <section>
+      <h1>ようこそ！日本100名城の旅へ</h1>
+      <p>まずは「地図で探す」から、行きたい城を見つけましょう。</p>
+      <p><a href="/map/">→ 地図で探す</a></p>
+    </section>
+  )
+}
